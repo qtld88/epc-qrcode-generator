4ff06a911516def1fc1366d38b7ac7344f9805c0
qtld <qtld@pm.me>
2026-06-01 10:09:37 +0200

    feat(api): add /groups endpoint and history share route

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 return [
 	'routes' => [
 		['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
+		['name' => 'page#groups', 'url' => '/groups', 'verb' => 'GET'],
 		['name' => 'history#index', 'url' => '/history', 'verb' => 'GET'],
 		['name' => 'history#show', 'url' => '/history/{id}', 'verb' => 'GET'],
 		['name' => 'history#create', 'url' => '/history', 'verb' => 'POST'],
+		['name' => 'history#share', 'url' => '/history/{id}/share', 'verb' => 'POST'],
 		['name' => 'history#destroy', 'url' => '/history/{id}', 'verb' => 'DELETE'],
 		['name' => 'preset#index', 'url' => '/presets', 'verb' => 'GET'],
 		['name' => 'preset#show', 'url' => '/presets/{id}', 'verb' => 'GET'],
